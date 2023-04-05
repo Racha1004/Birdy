6aaca0ee4999d8f500980203b1b3d606e1ff8874
David Tsolenyanu <davidtsolenyanu@MacBook-Air-de-David.local>
2023-04-05 09:18:55 +0200

    premiere requete test PostMan

---
+++ Serveur/Controllers/auth.controller.js
@@ -0,0 +1,13 @@
+const User = require('../models/user.model');
+
+module.exports.signUp = async(req, res) => {
+    console.log(req.body);
+    const {pseudo, email, password} = req.body;
+    try {
+        const user = await UserModel.create({pseudo, email, password});
+        res.status(201).json({user : user._id});
+    } catch (error) {
+        //res.status(500).json(error);
+        res.status(200).send("User already exists or ", { error })
+    }
+    }
