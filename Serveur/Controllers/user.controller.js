c0cac7b58edad58459217f8ad8d9f6b5c58f8b07
David Tsolenyanu <davidtsolenyanu@MacBook-Air-de-David.local>
2023-04-06 15:52:12 +0200

    follow,unfollow et delete user ajouté et marche bien

--- Serveur/Controllers/user.controller.js
+++ Serveur/Controllers/user.controller.js
@@ -42,4 +42,69 @@ module.exports.updateUser = async (req, res) => {
     }
 };
 
+module.exports.deleteUser = async (req, res) => {
+    if(!ObjectId.isValid(req.params.id))
+        return res.status(400).send('ID inconnu : ' + req.params.id)
 
+    try {
+        await UserModel.deleteOne({_id: req.params.id}).exec();        
+        res.status(200).json({message: "Successfully deleted."});
+
+    } catch(err) {
+        //console.log('ID inconnu : ' + err);
+        return res.status(500).json({message : err});
+    }
+}
+
+module.exports.follow = async (req, res) => {
+    if (!ObjectId.isValid(req.params.id) || !ObjectId.isValid(req.body.idToFollow)) {
+      return res.status(400).send('ID inconnu : ' + req.params.id);
+    }
+  
+    try {
+      // Ajouter à la liste des abonnés
+      const user = await UserModel.findByIdAndUpdate(
+        req.params.id,
+        { $addToSet: { following: req.body.idToFollow } },
+        { new: true, upsert: true }
+      ).exec();
+  
+      // Ajouter à la liste des abonnements
+      const followedUser = await UserModel.findByIdAndUpdate(
+        req.body.idToFollow,
+        { $addToSet: { followers: req.params.id } },
+        { new: true, upsert: true }
+      ).exec();
+  
+      return res.status(201).json({ user, followedUser });
+    } catch (err) {
+      return res.status(500).json({ message: err.message });
+    }
+};
+
+module.exports.unfollow = async (req, res) => {
+    if (!ObjectId.isValid(req.params.id) || !ObjectId.isValid(req.body.idToUnFollow)) {
+        return res.status(400).send('ID inconnu : ' + req.params.id);
+    }
+    
+    try{
+        // Retirer à la liste des abonnés
+        const user = await UserModel.findByIdAndUpdate(
+            req.params.id,
+            { $pull: { following: req.body.idToUnFollow } },
+            { new: true, upsert: true }
+          ).exec();
+      
+          // Retirer à la liste des abonnements
+          const unfollowedUser = await UserModel.findByIdAndUpdate(
+            req.body.idToUnFollow,
+            { $pull: { followers: req.params.id } },
+            { new: true, upsert: true }
+          ).exec();
+
+          // Retourner les deux objets mis à jour
+          return res.json({ user, unfollowedUser });
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+};
